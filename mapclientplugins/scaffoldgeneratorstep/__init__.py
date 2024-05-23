30f1724c22d9576cba2d476fb2f1835cb09ccd39
Kay <kayvvw@gmail.com>
2024-05-23 15:43:46 +1200

    init plugin

---
+++ mapclientplugins/scaffoldgeneratorstep/__init__.py
@@ -0,0 +1,13 @@
+
+"""
+MAP Client Plugin - Generated from MAP Client v0.20.0
+"""
+
+__version__ = '0.1.0'
+__author__ = 'Kay Wang'
+__stepname__ = 'scaffold generator'
+__location__ = 'https://github.com/mapclient-plugins/mapclientplugins.scaffoldgenerator'
+
+# import class that derives itself from the step mountpoint.
+from mapclientplugins.scaffoldgeneratorstep import step
+
