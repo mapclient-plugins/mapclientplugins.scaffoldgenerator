e2905b67fc4fcec1c9302db296b3f8da4a8faf36
Hugh Sorby <h.sorby@auckland.ac.nz>
2024-05-27 20:42:03 +1200

    Set version to 0.1.1.

--- mapclientplugins/scaffoldgeneratorstep/__init__.py
+++ mapclientplugins/scaffoldgeneratorstep/__init__.py
@@ -3,7 +3,7 @@
 MAP Client Plugin - Generated from MAP Client v0.20.0
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Kay Wang'
 __stepname__ = 'scaffold generator'
 __location__ = 'https://github.com/mapclient-plugins/mapclientplugins.scaffoldgenerator'
